447f5ea05c86972229b8a3194182de155610cae0
Azer0s <ariel@simulevski.at>
2021-11-03 12:01:46 +0100

    Add more GC unit tests

--- test.cpp
+++ test.cpp
@@ -95,12 +95,118 @@ TEST_CASE("Realloc allocated memory") {
     REQUIRE(freed[1].size == (size_t) 7997);
 }
 
-TEST_CASE("GC unused pointers") {
-    //TODO
+TEST_CASE("GC unused stack pointer") {
+    pog_static_heap_init();
+    pog_gc_init(NULL, 0);
+
+    void* a = pog_malloc(1);
+    REQUIRE(alloced[0].size == (size_t) 1);
+    REQUIRE(freed[0].size == (size_t) 7999);
+
+    a = nullptr;
+    REQUIRE(alloced[0].size == (size_t) 1);
+    REQUIRE(freed[0].size == (size_t) 7999);
+
+    pog_gc_collect();
+    REQUIRE(freed[0].size == (size_t) 1);
+    REQUIRE(freed[1].size == (size_t) 7999);
+
+    pog_squash();
+    REQUIRE(freed[0].size == (size_t) 8000);
 }
 
-TEST_CASE("GC stack pointers that have been set to null") {
-    //TODO
+TEST_CASE("GC multiple unused stack pointers") {
+    pog_static_heap_init();
+    pog_gc_init(NULL, 0);
+
+    void* a = pog_malloc(1);
+    void* b = pog_malloc(1);
+    void* c = pog_malloc(1);
+    REQUIRE(alloced[0].size == (size_t) 1);
+    REQUIRE(alloced[1].size == (size_t) 1);
+    REQUIRE(alloced[2].size == (size_t) 1);
+    REQUIRE(freed[0].size == (size_t) 7997);
+
+    a = nullptr;
+    REQUIRE(alloced[0].size == (size_t) 1);
+    REQUIRE(freed[0].size == (size_t) 7997);
+
+    pog_gc_collect();
+    REQUIRE(alloced[0].size == (size_t) 1);
+    REQUIRE(alloced[1].size == (size_t) 1);
+    REQUIRE(freed[0].size == (size_t) 1);
+    REQUIRE(freed[1].size == (size_t) 7997);
+
+    b = nullptr;
+    c = nullptr;
+    REQUIRE(alloced[0].size == (size_t) 1);
+    REQUIRE(alloced[1].size == (size_t) 1);
+
+    pog_gc_collect();
+    REQUIRE(freed[0].size == (size_t) 1);
+    REQUIRE(freed[1].size == (size_t) 1);
+    REQUIRE(freed[2].size == (size_t) 1);
+    REQUIRE(freed[3].size == (size_t) 7997);
+
+    pog_squash();
+    REQUIRE(freed[0].size == (size_t) 8000);
+}
+
+TEST_CASE("GC pointers in heap without GC root") {
+    pog_static_heap_init();
+    pog_gc_init(NULL, 0);
+
+    struct LinkedList {
+        int val;
+        LinkedList* next;
+    };
+    auto a = (LinkedList*) pog_malloc(sizeof(LinkedList));
+    auto b = (LinkedList*) pog_malloc(sizeof(LinkedList));
+    auto c = (LinkedList*) pog_malloc(sizeof(LinkedList));
+    auto d = (LinkedList*) pog_malloc(sizeof(LinkedList));
+
+    a->val = 1;
+    a->next = b;
+
+    b->val = 2;
+    b->next = c;
+
+    c->val = 3;
+    c->next = d;
+
+    d->val = 4;
+
+    REQUIRE(alloced[0].size == (size_t) 2);
+    REQUIRE(alloced[1].size == (size_t) 2);
+    REQUIRE(alloced[2].size == (size_t) 2);
+    REQUIRE(alloced[3].size == (size_t) 2);
+    REQUIRE(freed[0].size == (size_t) 7992);
+
+    b->next = nullptr;
+    c = nullptr;
+    d = nullptr;
+
+    pog_gc_collect();
+    REQUIRE(alloced[0].size == (size_t) 2);
+    REQUIRE(alloced[1].size == (size_t) 2);
+    REQUIRE(freed[0].size == (size_t) 2);
+    REQUIRE(freed[1].size == (size_t) 2);
+    REQUIRE(freed[2].size == (size_t) 7992);
+
+    pog_squash();
+    REQUIRE(alloced[0].size == (size_t) 2);
+    REQUIRE(alloced[1].size == (size_t) 2);
+    REQUIRE(freed[0].size == (size_t) 7996);
+
+    a = nullptr;
+    b = nullptr;
+    pog_gc_collect();
+    REQUIRE(freed[0].size == (size_t) 2);
+    REQUIRE(freed[1].size == (size_t) 2);
+    REQUIRE(freed[2].size == (size_t) 7996);
+
+    pog_squash();
+    REQUIRE(freed[0].size == (size_t) 8000);
 }
 
 TEST_CASE("GC static pointers that have been set to null") {
